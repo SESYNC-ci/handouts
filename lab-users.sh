9f31761c7e81dec9a32f755bedef03df8eb20564
itcarroll <icarroll@sesync.org>
2019-07-19 19:40:01 -0400

    location

--- lab-users.sh
+++ lab-users.sh
@@ -5,5 +5,5 @@ while read USER PASS; do
 	PASS=$(curl https://frightanic.com/goodies_content/docker-names.php)
     fi
     printf "%s %s\n" "$USER" "$PASS"
-done < root/tmp/lab-users.txt > root/tmp/lab-users.txt.tmp
-mv root/tmp/lab-users.txt.tmp root/tmp/lab-users.txt
+done < root/tmp/lab/users.txt > root/tmp/lab/users.txt.tmp
+mv root/tmp/lab/users.txt.tmp root/tmp/lab/users.txt
